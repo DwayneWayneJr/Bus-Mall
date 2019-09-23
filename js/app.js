d9123de9cb220d916243b3e46ed0a7547d1cc7fb
DwayneWayneJr <dwaynejgrant@gmail.com>
2019-09-23 14:14:24 -0700

    working to complete problems for user story 1

--- js/app.js
+++ js/app.js
@@ -1,7 +1,30 @@
 // 'use strict';
 
-function Goat(name, image){
+// global variables
+var
+
+//  constructor Function for object assoctiated with each product
+function FavoriteItem(name, image){
   this.name = name;
   this.image = image;
-  
+  this.clicked = 0;
+  this.views = 0;
+
+  // logic of pushing images
+  FavoriteItem.allimages.push(this);
 }
+
+// create random function
+// to select from array of images
+// return random number between 0-1
+function randomFavoriteItem(){
+  var randomNumber = Math.floor(Math.random() * randomFavoriteItem.length);
+  return randomNumber;
+}
+
+// create function to render to screen
+function renderFavoriteItem(){
+
+  // create variables to distinguish from each picture
+}
+

e0eae64b6598890e5f89959c7f7c881e20238385
DwayneWayneJr <dwaynejgrant@gmail.com>
2019-09-23 14:53:30 -0700

    html with image tags setup, render function and do while loop set up, global variables created

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 // 'use strict';
 
 // global variables
-var
+var favoriteItemImage = document.getElementById('favoriteImages');
+var leftFavorite = document.getElementById('leftFavorite');
+var centerFavorite = document.getElementById('fcenterFavorite');
+var rightFavorite = document.getElementById('rightFavorite');
+var leftFavoriteIndex = null;
+var centerFavoriteIndex = null;
+var rightFavoriteIndex = null;
+
 
 //  constructor Function for object assoctiated with each product
-function FavoriteItem(name, image){
+function FavoriteItem(name, image) {
   this.name = name;
   this.image = image;
   this.clicked = 0;
@@ -17,14 +24,20 @@ function FavoriteItem(name, image){
 // create random function
 // to select from array of images
 // return random number between 0-1
-function randomFavoriteItem(){
+function randomFavoriteItem() {
   var randomNumber = Math.floor(Math.random() * randomFavoriteItem.length);
   return randomNumber;
 }
 
 // create function to render to screen
-function renderFavoriteItem(){
+function renderFavoriteItem() {
 
-  // create variables to distinguish from each picture
+  // create variables to distinguish from each picture and give random images while a certain condition is true
+  do {
+    leftFavoriteIndex = randomFavoriteItem();
+    centerFavoriteIndex = randomFavoriteItem();
+    rightFavoriteIndex = randomFavoriteItem();
+  } while (leftFavoriteIndex === centerFavoriteIndex || centerFavoriteIndex === rightFavoriteIndex || rightFavoriteIndex === leftFavoriteIndex);
 }
 
+renderFavoriteItem();

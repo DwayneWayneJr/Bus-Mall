00691db6a83d6db210bc9856ec1f9b924172d5f7
DwayneWayneJr <dwaynejgrant@gmail.com>
2019-09-24 15:25:03 -0700

    added graph to put into lab 12

--- js/app.js
+++ js/app.js
@@ -30,10 +30,29 @@ function FavoriteItem(name, image) {
 // to select from array of images
 // return random number between 0-1
 function randomFavoriteItem() {
-  var randomNumber = Math.floor(Math.random() * allImages.length);
+  var randomFavoriteItem = Math.floor(Math.random() * allImages.length);
   console.log(allImages.length);
-  return randomNumber;
-}
+  return randomFavoriteItem;
+};
+
+// function displayImage(){
+//   var randomImages = [];
+//   randomImages[0] = randomFavoriteItem();
+//   randomImages[1] = randomFavoriteItem();
+
+//   while(randomImages[0] === randomImages[1]) {
+//     console.log('double trouble');
+//     randomImages[1] = randomFavoriteItem();
+//   }
+//   randomImages[2] = randomFavoriteItem();
+//   while(randomImages[2] === randomImages[1] || randomImages[2] === randomImages[0]){
+//     console.log('double trouble');
+//     randomImages[2] = randomFavoriteItem();
+//   }
+//   for(var i = 0; i < 3; i++) {
+
+//   }
+// }
 
 // create function to render to screen
 function renderFavoriteItem() {
@@ -129,3 +148,41 @@ renderFavoriteItem();
 
 favoriteItemImage.addEventListener('click', clickedOnFavorite);
 // console.log(allImages);
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});

5a113531e7f1ad8a98c841abc60b06f46f3b0bd8
DwayneWayneJr <dwaynejgrant@gmail.com>
2019-09-23 20:47:23 -0700

    images not rendering

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
 // 'use strict';
 
-// global variables
+// global variable
 var favoriteItemImage = document.getElementById('favoriteImages');
 var leftFavorite = document.getElementById('leftFavorite');
 var centerFavorite = document.getElementById('centerFavorite');
 var rightFavorite = document.getElementById('rightFavorite');
+
 var leftFavoriteIndex = null;
 var centerFavoriteIndex = null;
 var rightFavoriteIndex = null;
 
 var favoriteVote = 0;
 
+FavoriteItem.allimages = [];
 
 //  constructor Function for object assoctiated with each product
 function FavoriteItem(name, image) {
@@ -20,83 +22,89 @@ function FavoriteItem(name, image) {
   this.views = 0;
 
   // logic of pushing images
-  FavoriteItem.allimages.push(this);
+  FavoriteItem.allImages.push(this);
+
 }
 
 // create random function
 // to select from array of images
 // return random number between 0-1
 function randomFavoriteItem() {
-  var randomNumber = Math.floor(Math.random() * randomFavoriteItem.length);
+  var randomNumber = Math.floor(Math.random() * FavoriteItem.allImages.length);
   return randomNumber;
 }
 
 // create function to render to screen
 function renderFavoriteItem() {
-
   // create variables to distinguish from each picture and give random images while a certain condition is true
   do {
     leftFavoriteIndex = randomFavoriteItem();
     centerFavoriteIndex = randomFavoriteItem();
     rightFavoriteIndex = randomFavoriteItem();
+    console.log(leftFavoriteIndex);
+    console.log(centerFavoriteIndex);
+    console.log(rightFavoriteIndex);
   } while (leftFavoriteIndex === centerFavoriteIndex || centerFavoriteIndex === rightFavoriteIndex || rightFavoriteIndex === leftFavoriteIndex);
 
+  FavoriteItem.allimages[leftFavoriteIndex].views++;
+  FavoriteItem.allimages[centerFavoriteIndex].views++;
+  FavoriteItem.allimages[rightFavoriteIndex].views++;
+
   // source of the image tags to the specific picture of the array
-  leftFavorite.src = FavoriteItem.allimages[leftFavoriteIndex].image;
-  centerFavorite.src = FavoriteItem.allimages[centerFavoriteIndex].image;
-  rightFavorite.src = FavoriteItem.allimages[rightFavoriteIndex].image;
+  leftFavorite.src = FavoriteItem.allImages[leftFavoriteIndex].image;
+  centerFavorite.src = FavoriteItem.allImages[centerFavoriteIndex].image;
+  rightFavorite.src = FavoriteItem.allImages[rightFavoriteIndex].image;
+  // function expression
+  var clickedOnFavorite = function () {
+    var favoriteClicked = event.target.id;
+
+    if (favoriteClicked === 'leftFavorite' || favoriteClicked === 'centerFavorite' || favoriteClicked === 'rightFavorite') {
+
+      favoriteVote++;
+      // incrament favoritre clicked by one
+      if (favoriteClicked === 'leftFavorite') {
+        // do logic to increment number
+        FavoriteItem.allImages[leftFavoriteIndex].clicked++;
+      } else if (favoriteClicked === 'centerFavorite') {
+        FavoriteItem.allImages[centerFavoriteIndex].clicked++;
+      } else if (favoriteClicked === 'rightFavorite') {
+        FavoriteItem.allImages[rightFavoriteIndex].clicked++;
+      }
+    } else {
+      alert('You didn\'t select on image');
+    }
+
+    console.log(FavoriteItem.allImages[leftFavoriteIndex]);
+
+    console.log(FavoriteItem.allImages[centerFavoriteIndex]);
+
+    console.log(FavoriteItem.allImages[rightFavoriteIndex]);
+
+    // check votes
+    if (favoriteVote === 5) {
+      favoriteItemImage.removeEventListener('click', clickedOnFavorite);
+
+      console.log('You competed the voting.');
+
+      // output to browser
+      for (var i = 0; i < FavoriteItem.allImages.length; i++) {
+        var FavoriteItem = FavoriteItem.allImages[i];
+      }
+    } else {
+      renderFavoriteItem();
+    }
+
+  };
+
+  FavoriteItem.allImages = [];
+
+  new FavoriteItem('bag', 'images/bag.jpg');
+  new FavoriteItem('banana', 'images/banana.jpg');
+  new FavoriteItem('bathroom', 'images/bathroom.jpg');
+
+  console.log(FavoriteItem.allImages);
+  renderFavoriteItem();
+
+  favoriteItemImage.addEventListener('click', clickedOnFavorite);
+
 }
-
-// function expression
-var clickedOnFavorite = function(event) {
-  var favoriteClicked = event.target.id;
-
-  if (favoriteClicked === 'leftFavorite' || favoriteClicked === 'centerFavorite' || favoriteClicked === 'rightFavorite'){
-    favoriteVote++;
-    // incrament favoritre clicked by one
-    if (favoriteClicked === 'leftFavorite') {
-      // do logic to increment number
-      FavoriteItem.allimages[leftFavoriteIndex].clicked++;
-    } else if (favoriteClicked === 'centerFavorite') {
-      FavoriteItem.allimages[centerFavoriteIndex].clicked++;
-    } else if (favoriteClicked === 'rightFavorite') {
-      FavoriteItem.allimages[rightFavoriteIndex].clicked++;
-    }
-  } else {
-    alert('You didn\'t select on image');
-  }
-
-  console.log(FavoriteItem.allimages[leftFavoriteIndex]);
-
-  console.log(FavoriteItem.allimages[centerFavoriteIndex]);
-
-  console.log(FavoriteItem.allimages[rightFavoriteIndex]);
-
-  // check votes
-  if (favoriteVote === 5) {
-    favoriteItemImage.removeEventListener('click', clickedOnFavorite);
-
-    console.log('You competed the voting.');
-
-    // output to browser
-    for (var i = 0; i < FavoriteItem.allimages.length; i++)
-    {
-      var favorite = FavoriteItem.allimages[i];
-    }
-  } else {
-    renderFavoriteItem();
-  }
-
-};
-
-FavoriteItem.allimages = [];
-
-new FavoriteItem('bag', 'images/bag'.jpg.url);
-new FavoriteItem('banana', 'images/banana.jpg.url');
-
-console.log(FavoriteItem.allimages);
-renderFavoriteItem();
-
-favoriteItemImage.addEventListener('click', clickedOnFavorite);
-
-
